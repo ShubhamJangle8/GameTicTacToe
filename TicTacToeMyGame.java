ff8bee4ea88d3d040236bc9972a548121681e2b7
jangle <shubhamjangale0000@gmail.com>
2020-10-10 15:02:59 +0530

    Refactor : Added functionality to get a position from a player he wants to move to!

--- TicTacToeMyGame.java
+++ TicTacToeMyGame.java
@@ -3,7 +3,7 @@ package com.game.myNewGame;
 import java.util.Scanner;
 
 public class TicTacToeMyGame {	
-	char[] board;
+	static char[] board;
 	
 	//UC1
 	public TicTacToeMyGame() {
@@ -30,8 +30,20 @@ public class TicTacToeMyGame {
 			System.out.print(" | ");
 		}
 	}
+	//UC4
+	public boolean moveTo(int position, char playerSymbol) {
+		if(board[position] == ' ') {
+			board[position] = playerSymbol;
+			return true;
+		}
+		else {
+			System.out.println("Enter a valid position! Place already filled !");
+			return false;
+		}	
+	}
 	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		char playerSymbol;
 		char compSymbol;
 		System.out.println("Welcome to the TicTacToe game");
@@ -49,5 +61,18 @@ public class TicTacToeMyGame {
 		System.out.println("Your symbol is " + playerSymbol);
 		System.out.println("Computer symbol is " + compSymbol);
 		game.showBoard();
+		while(true) {
+			System.out.println("Player enter the position you want to move to : ");
+			int position = input.nextInt();
+			if(position < 1 && position > 9) {
+				System.out.println("Enter valid position");
+				continue;
+			}
+			if(game.moveTo(position, playerSymbol)) {
+				game.showBoard();
+				break;
+			}
+		}
+		input.close();
 	}	
 }
