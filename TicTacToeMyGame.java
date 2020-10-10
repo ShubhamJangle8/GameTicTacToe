a3757a45b150e69b8325369a533bcfab39c99f84
jangle <shubhamjangale0000@gmail.com>
2020-10-10 11:38:30 +0530

    Refactor : Added functionality to create a New board

--- TicTacToeMyGame.java
+++ TicTacToeMyGame.java
@@ -1,7 +1,14 @@
 package com.game.myNewGame;
 
 public class TicTacToeMyGame {	
+	char[] board;
+	
+	public TicTacToeMyGame() {
+		board = new char[10];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the TicTacToe game");
+		TicTacToeMyGame game = new TicTacToeMyGame();
 	}	
 }
