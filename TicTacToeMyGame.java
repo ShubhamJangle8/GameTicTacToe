82a8b66c4af3afeefd08ea8fddfdfad9ee4aab00
jangle <shubhamjangale0000@gmail.com>
2020-10-10 11:59:54 +0530

    Refactor : Added functionality to choose a symbol for a player

--- TicTacToeMyGame.java
+++ TicTacToeMyGame.java
@@ -1,14 +1,39 @@
 package com.game.myNewGame;
 
+import java.util.Scanner;
+
 public class TicTacToeMyGame {	
 	char[] board;
 	
+	
 	public TicTacToeMyGame() {
 		board = new char[10];
+		for(int i = 0; i < board.length; i++) {
+			board[i] = ' ';
+		}
+	}
+	
+	public char getSymbol() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Play with X/O ? ");
+		char symbol = input.next().charAt(0);
+		return symbol;
 	}
 	
 	public static void main(String[] args) {
+		char playerSymbol;
+		char compSymbol;
 		System.out.println("Welcome to the TicTacToe game");
 		TicTacToeMyGame game = new TicTacToeMyGame();
+		while(true) {
+			playerSymbol = game.getSymbol();
+			compSymbol = (playerSymbol == 'X') ? 'O' : 'X';	
+			if(playerSymbol == 'X' || playerSymbol == 'O') {
+				break;
+			}
+			else {
+				System.out.println("Enter a valid Character");
+			}
+		}
 	}	
 }
