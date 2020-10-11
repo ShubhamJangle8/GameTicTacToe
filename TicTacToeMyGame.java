3924e5390fcc6f0e85823f45f34d91bc59ae47d6
jangle <shubhamjangale0000@gmail.com>
2020-10-11 11:59:03 +0530

    Refactor : Added functionality to select the corner if not present, then center if not center, then sides

--- TicTacToeMyGame.java
+++ TicTacToeMyGame.java
@@ -88,15 +88,25 @@ public class TicTacToeMyGame {
 					}
 				}
 				System.out.println("Empty corners : " + countEmptyCorners);
-				// if no one is winning and all corners are filled then selecting random position
+				// if no one is wining then selecting corner
 				if (countEmptyCorners == 0) {
-					position = (int) Math.floor(Math.random() * 10) % 9 + 1;
+					// if corner is not available then taking center
+					if (board[5] == ' ') {
+						position = 5;
+					}
+					// if center is not available then taking side
+					else {
+						while (true) {
+							position = (int) Math.floor(Math.random() * 10) % 9 + 1;
+							if (board[position] == ' ')
+								break;
+						}
+					}
 				}
 				// if corner is empty then selecting random corner
 				else {
 					while (true) {
 						position = corners[(int) Math.floor(Math.random() * 10) % 4];
-						System.out.println("Corner selected : " + position);
 						if (board[position] == ' ')
 							break;
 					}
